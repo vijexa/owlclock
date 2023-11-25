43717a95096104ed0a2e69cf7d669bd9c9dc750f
vijexa <nuzhdinvitja@gmail.com>
2023-11-25 10:25:26 +0200

    Fix indent

--- src/info/InfoPage.tsx
+++ src/info/InfoPage.tsx
@@ -4,49 +4,49 @@ import CardHeader from "@mui/material/CardHeader";
 import Divider from "@mui/material/Divider";
 
 export function InfoPage() {
-    return (
-        <>
-            <Card sx={{ boxShadow: "none" }}>
-                <CardHeader
-                    title="OwlClock🦉 Information"
-                    titleTypographyProps={{
-                        sx: {
-                            fontSize: "1.125rem",
-                            fontWeight: "bold",
-                            lineHeight: "32px",
-                            color: "text.primary",
-                        },
-                    }}
-                />
-                <Divider variant="middle" />
-                <CardContent>
-                    <Typography
+  return (
+    <>
+      <Card sx={{ boxShadow: "none" }}>
+        <CardHeader
+          title="OwlClock🦉 Information"
+          titleTypographyProps={{
+            sx: {
+              fontSize: "1.125rem",
+              fontWeight: "bold",
+              lineHeight: "32px",
+              color: "text.primary",
+            },
+          }}
+        />
+        <Divider variant="middle" />
+        <CardContent>
+          <Typography
 
-                    >
-                        This extension provides a simple way to keep track of in-game time during the day.
-                    </Typography>
-                    <br />
-                    <Typography>
-                        You can edit the time directly by editing it in the text field, or by using the "Add" field
-                        and "Hours"/"Minutes" buttons. The time will be saved in room data. Only GM can edit the time.
-                        Temporarily only 24-hour (ISO) time format is supported.
-                    </Typography>
-                    <br />
-                    <Typography>
-                        If there are any bugs, feel free to <a href="https://github.com/vijexa/owlclock/issues/new" target="_blank">open an issue</a> on GitHub. Contributions are welcome!
-                    </Typography>
-                </CardContent>
-                <CardActions>
+          >
+            This extension provides a simple way to keep track of in-game time during the day.
+          </Typography>
+          <br />
+          <Typography>
+            You can edit the time directly by editing it in the text field, or by using the "Add" field
+            and "Hours"/"Minutes" buttons. The time will be saved in room data. Only GM can edit the time.
+            Temporarily only 24-hour (ISO) time format is supported.
+          </Typography>
+          <br />
+          <Typography>
+            If there are any bugs, feel free to <a href="https://github.com/vijexa/owlclock/issues/new" target="_blank">open an issue</a> on GitHub. Contributions are welcome!
+          </Typography>
+        </CardContent>
+        <CardActions>
 
-                    <Button
-                        startIcon={<GitHub />}
-                        sx={{ color: 'text.primary' }}
-                        href="https://github.com/vijexa/owlclock"
-                        target="_blank" >
-                        GitHub
-                    </Button>
-                </CardActions>
-            </Card >
-        </>
-    );
+          <Button
+            startIcon={<GitHub />}
+            sx={{ color: 'text.primary' }}
+            href="https://github.com/vijexa/owlclock"
+            target="_blank" >
+            GitHub
+          </Button>
+        </CardActions>
+      </Card >
+    </>
+  );
 }
