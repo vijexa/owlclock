ffb7e65ae218e1419ac85a8129214e1f80a53a90
vijexa <nuzhdinvitja@gmail.com>
2023-12-09 15:03:53 +0200

    Implement custom modal

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import { IconButton } from "@mui/material";
 import CardHeader from "@mui/material/CardHeader";
 import Divider from "@mui/material/Divider";
 import OBR from "@owlbear-rodeo/sdk";
-import { INFO_MODAL_ID, SETTINGS_MODAL_ID } from "./modal";
+import { INFO_MODAL_ID, SETTINGS_MODAL_ID } from "./modal/modal";
 
 interface HeaderProps {
   isGm: boolean;
@@ -27,8 +27,8 @@ export function Header({ isGm }: HeaderProps) {
             onClick={() => OBR.modal.open({
               id: INFO_MODAL_ID,
               url: "/#/info",
-              height: 660,
-              width: 300,
+              hidePaper: true,
+              fullScreen: true,
             })}
           >
             <InfoOutlined></InfoOutlined>
@@ -38,10 +38,8 @@ export function Header({ isGm }: HeaderProps) {
               id: SETTINGS_MODAL_ID,
               // passing isGm as a query param for more consistent rendering
               url: "/#/settings?isGm=" + isGm,
-              width: 300,
-              // there is no way to resize the modal height...
-              height: isGm ? 450 : 220,
               hidePaper: true,
+              fullScreen: true,
             })}
           >
             <SettingsOutlined></SettingsOutlined>
--- src/settings/SettingsPage.tsx
+++ src/settings/SettingsPage.tsx
@@ -1,19 +1,21 @@
 import { HelpOutline } from "@mui/icons-material";
-import { Box, Button, Card, CardActions, CardContent, FormControl, FormLabel, IconButton, Slider, Switch, ToggleButton, ToggleButtonGroup, Tooltip, styled } from "@mui/material";
+import { Box, Button, CardActions, CardContent, FormControl, FormLabel, IconButton, Slider, Switch, ToggleButton, ToggleButtonGroup, Tooltip, styled } from "@mui/material";
 import CardHeader from "@mui/material/CardHeader";
 import Divider from "@mui/material/Divider";
 import OBR from "@owlbear-rodeo/sdk";
 import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { ModalCard } from "../modal/ModalCard";
+import { SETTINGS_MODAL_ID } from "../modal/modal";
 import { TimeFormat } from "../time";
 import { getSavedChangeDateOnTextInput, getSavedHistorySize, getSavedIntegrateWithCalendar, getSavedTimeFormat, saveSetttings } from "./settings";
-import { SETTINGS_MODAL_ID } from "../modal";
 
 const RowFormControl = styled(FormControl)({
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
-  justifyContent: 'flex-start'
+  justifyContent: 'flex-start',
+  gap: '8px',
 });
 const StyledFormLabel = styled(FormLabel)({ marginBottom: '8px' });
 const StyledToggleButton = styled(ToggleButton)({ paddingLeft: '12px', paddingRight: '12px' });
@@ -45,85 +47,83 @@ export function SettingsPage() {
   }
 
   return (
-    <>
-      <Card sx={{ boxShadow: 'none' }}>
-        <CardHeader
-          title="OwlClock🦉 Settings"
-          titleTypographyProps={{
-            sx: {
-              fontSize: "1.125rem",
-              fontWeight: "bold",
-              lineHeight: "32px",
-              color: "text.primary",
-            },
-          }}
-        />
-        <Divider variant="middle" />
-        <CardContent style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '16px',
-        }}>
-          <FormControl fullWidth>
-            <StyledFormLabel>Time Format</StyledFormLabel>
-            <ToggleButtonGroup
-              exclusive
-              size="small"
-              fullWidth
-              color="primary"
-              value={timeFormat}
-              onChange={processTimeFormatChange}
-            >
-              <StyledToggleButton value={TimeFormat.H24}>
-                24h
-              </StyledToggleButton>
-              <StyledToggleButton value={TimeFormat.H12}>
-                12h
-              </StyledToggleButton>
-            </ToggleButtonGroup>
-          </FormControl>
+    <ModalCard modalId={SETTINGS_MODAL_ID}>
+      <CardHeader
+        title="OwlClock🦉 Settings"
+        titleTypographyProps={{
+          sx: {
+            fontSize: "1.125rem",
+            fontWeight: "bold",
+            lineHeight: "32px",
+            color: "text.primary",
+          },
+        }}
+      />
+      <Divider variant="middle" />
+      <CardContent style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '16px',
+      }}>
+        <FormControl fullWidth>
+          <StyledFormLabel>Time Format</StyledFormLabel>
+          <ToggleButtonGroup
+            exclusive
+            size="small"
+            fullWidth
+            color="primary"
+            value={timeFormat}
+            onChange={processTimeFormatChange}
+          >
+            <StyledToggleButton value={TimeFormat.H24}>
+              24h
+            </StyledToggleButton>
+            <StyledToggleButton value={TimeFormat.H12}>
+              12h
+            </StyledToggleButton>
+          </ToggleButtonGroup>
+        </FormControl>
 
-          {
-            isGm
-              ? <>
-                <FormControl fullWidth>
-                  <StyledFormLabel>History Size</StyledFormLabel>
-                  <Box padding='0 8px'>
-                    <Slider
-                      value={historySize}
-                      marks
-                      min={1}
-                      max={10}
-                      valueLabelDisplay="auto"
-                      onChange={(_, value) => setHistorySize(value as number)}
-                    />
-                  </Box>
-                </FormControl>
-                <RowFormControl fullWidth>
-                  <Switch checked={integrateWithCalendar} onChange={e => setIntegrateWithCalendar(e.target.checked)} />
-                  <FormLabel focused={false}>Integrate with "Calendar!"?</FormLabel>
-                </RowFormControl>
-                <RowFormControl fullWidth style={{ gap: '0px' }} disabled={!integrateWithCalendar}>
-                  <Switch checked={changeDateOnTextInput} onChange={e => setChangeDateOnTextInput(e.target.checked)} />
-                  <FormLabel focused={false}>Change date on text input?</FormLabel>
-                  <Tooltip arrow title="This option enables date change also on text input. It will assume that if you enter time before current time, you must mean the next day. A hint will appear when applying the time will advance the date. Example: entering 8:00 when current time is 23:00 will trigger date change.">
-                    <IconButton>
-                      <HelpOutline></HelpOutline>
-                    </IconButton>
-                  </Tooltip>
-                </RowFormControl>
-              </>
-              : <></>
-          }
-        </CardContent>
+        {
+          isGm
+            ? <>
+              <FormControl fullWidth>
+                <StyledFormLabel>History Size</StyledFormLabel>
+                <Box padding='0 8px'>
+                  <Slider
+                    value={historySize}
+                    marks
+                    min={1}
+                    max={10}
+                    valueLabelDisplay="auto"
+                    onChange={(_, value) => setHistorySize(value as number)}
+                  />
+                </Box>
+              </FormControl>
+              <RowFormControl fullWidth>
+                <Switch checked={integrateWithCalendar} onChange={e => setIntegrateWithCalendar(e.target.checked)} />
+                <FormLabel focused={false}>Integrate with "Calendar!"?</FormLabel>
+              </RowFormControl>
+              <RowFormControl fullWidth disabled={!integrateWithCalendar}>
+                <Switch checked={changeDateOnTextInput} onChange={e => setChangeDateOnTextInput(e.target.checked)} />
+                <FormLabel focused={false}>Change date on text input?</FormLabel>
+                <Tooltip arrow title="This option enables date change also on text input. It will assume that if you enter time before current time, you must mean the next day. A hint will appear when applying the time will advance the date. Example: entering 8:00 when current time is 23:00 will trigger date change.">
+                  <IconButton>
+                    <HelpOutline></HelpOutline>
+                  </IconButton>
+                </Tooltip>
+              </RowFormControl>
+            </>
+            : <></>
+        }
+      </CardContent>
 
-        <CenteredCardActions >
-          <Button variant="outlined" onClick={() => onSaveClick(timeFormat, historySize, integrateWithCalendar, changeDateOnTextInput)}>
-            Save
-          </Button>
-        </CenteredCardActions>
-      </Card >
-    </>
+      <CenteredCardActions >
+        <Button variant="outlined" onClick={() => onSaveClick(timeFormat, historySize, integrateWithCalendar, changeDateOnTextInput)}>
+          Save
+        </Button>
+      </CenteredCardActions>
+    </ModalCard >
   );
 }
 
